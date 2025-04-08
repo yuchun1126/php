e62131a6ca2cfebde26552e23f84f076e2c6c11d
yuchun1126 <gennie931126@gmail.com>
2025-04-08 09:29:34 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>  <!-- 定義網頁標題 -->
+    <body>
+        <?php echo "Hello world!"; ?>  <!-- PHP 程式碼，輸出 "Hello world!" -->
+        <h1>html</h1>  <!-- 顯示 HTML 標題文字 "html" -->
+        <?php echo "Hello world!"; ?>  <!-- 再次輸出 "Hello world!" -->
+        <h1>html</h1>  <!-- 顯示 HTML 標題文字 "html" -->
+        <?php echo "Hello world!"; ?>  <!-- 再次輸出 "Hello world!" -->
+    </body>
+</html>
